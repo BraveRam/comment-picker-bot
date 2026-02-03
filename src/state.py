5de9b2cb6b956e9cfa3b057034ca22e219b36d64
BraveRam <99811385+BraveRam@users.noreply.github.com>
2026-02-03 16:18:06 +0300

    Remove all files from master branch

--- src/state.py
+++
@@ -1,7 +0,0 @@
-import time
-
-def prune_state(state_by_user, ttl_seconds: int):
-    now = time.time()
-    stale = [k for k, v in state_by_user.items() if now - v["ts"] > ttl_seconds]
-    for k in stale:
-        state_by_user.pop(k, None)
